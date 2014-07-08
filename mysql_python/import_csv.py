8ef46ce1a46746e9b4d00f87e038f60319be5f15
schmiani <schmiani@gmail.com>
2014-07-08 11:43:02 -0400

    Extend import to all 3 csv files

--- mysql_python/import_csv.py
+++ mysql_python/import_csv.py
@@ -8,13 +8,14 @@ mydb = MySQLdb.connect(host='localhost',
     db='daycare')
 cur = mydb.cursor()
 cur.execute('DROP TABLE IF EXISTS Basic')
+cur.execute('DROP TABLE IF EXISTS Inspections')
+cur.execute('DROP TABLE IF EXISTS InspectionSum')
 
-
-info = csv.reader(file('../python_combine_and_geocode/BasicInfo_2014_06_12.csv'))
+info = csv.reader(file('../python_scraper/BasicInfo_2014_06_12.csv'))
 header = info.next()
 headersql = [s.replace(' ', '_') for s in header]
 
-query = 'CREATE TABLE Basic('+'%s VARCHAR(255), ' * (len(header)-1)+'%s VARCHAR(255))'
+query = 'CREATE TABLE Basic('+'%s TEXT, ' * (len(header)-1)+'%s TEXT)'
 query = query % tuple(headersql)
 cur.execute(query)
 
@@ -23,6 +24,38 @@ query = query % tuple(headersql)
 query = query + ' VALUES(' + '%s, ' *(len(header)-1) + '%s)'
 for row in info:
 	cur.execute(query, row)
+
+mydb.commit()
+
+info = csv.reader(file('../python_scraper/InspectionInfo_2014_06_12.csv'))
+header = info.next()
+headersql = [s.replace(' ', '_').replace('-','_') for s in header]
+
+query = 'CREATE TABLE Inspections('+'%s TEXT, ' * (len(header)-1)+'%s TEXT)'
+query = query % tuple(headersql)
+cur.execute(query)
+
+query = 'INSERT INTO Inspections(' + '%s, ' *(len(header)-1) + '%s)'
+query = query % tuple(headersql)
+query = query + ' VALUES(' + '%s, ' *(len(header)-1) + '%s)'
+for row in info:
+    cur.execute(query, row)
+
+mydb.commit()
+
+info = csv.reader(file('../python_scraper/InspectionSummaries_2014_06_12.csv'))
+header = info.next()
+headersql = [s.replace(' ', '_').replace('-','_') for s in header]
+
+query = 'CREATE TABLE InspectionSum('+'%s TEXT, ' * (len(header)-1)+'%s TEXT)'
+query = query % tuple(headersql)
+cur.execute(query)
+
+query = 'INSERT INTO InspectionSum(' + '%s, ' *(len(header)-1) + '%s)'
+query = query % tuple(headersql)
+query = query + ' VALUES(' + '%s, ' *(len(header)-1) + '%s)'
+for row in info:
+    cur.execute(query, row)
 
 mydb.commit()
 cur.close()
