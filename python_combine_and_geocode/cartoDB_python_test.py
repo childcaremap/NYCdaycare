44675f142334ce4079189ff0aca02904e2e05a78
hpf <hafrey@gmail.com>
2014-07-10 10:56:48 -0400

    Multirow insert

--- python_combine_and_geocode/cartoDB_python_test.py
+++ python_combine_and_geocode/cartoDB_python_test.py
@@ -1,8 +1,14 @@
 from cartodb import CartoDBOAuth, CartoDBException
 from secret import *
+import json
 
 cl = CartoDBOAuth(CONSUMER_KEY, CONSUMER_SECRET, user, password, cartodb_domain)
 try:
-    print cl.sql("select * from basicinfo where site_id='DC20687'")
+    a = cl.sql("SELECT * FROM basicinfo WHERE site_id='DC20687'")   # WHERE site_id='DC20687'
+
+    print(a)
+
+    #for key, value in a.items():
+        #print(key, value)
 except CartoDBException as e:
     print ("some error ocurred", e)
