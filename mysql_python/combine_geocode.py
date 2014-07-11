48018741183d50b49d6d1730324ef40df297514f
schmiani <schmiani@gmail.com>
2014-07-11 16:47:13 -0400

    Newer version of summary data

---
+++ mysql_python/combine_geocode.py
@@ -0,0 +1,59 @@
+import csv
+import time
+import sys
+from pygeocoder import Geocoder
+
+def combine_geocode(filename):
+    with open(filename, 'rb') as input_file:
+        info = csv.reader(input_file)
+        header = info.next()
+        iaddress = header.index('Address')
+        iborough = header.index('Borough')
+        izip= header.index('Zip_Code')
+        
+        k = 0
+        newrows = []  # Initialize clean rows
+        # retrieve geolocation from Google API
+        for i, row in enumerate(info):
+            if row[iborough] == 'MANHATTAN':
+                address =  row[iaddress] +', NEW YORK, NY ' + row[izip]
+            else:
+                address = row[iaddress] +', ' + row[iborough] + ', NY ' + row[izip]
+            print i
+            try:
+                result = Geocoder.geocode(address)
+                time.sleep(.5)
+                if len(result) == 1:
+                    row.append(result.coordinates[0])
+                    row.append(result.coordinates[1])
+                    newrows.append(row)
+                else:
+                    print 'more than one geolocation found for ' + address
+                    row.append(result[0].coordinates[0])
+                    row.append(result[0].coordinates[1])
+                    newrows.append(row)
+            except:
+                k = k + 1
+                row.append(None)
+                row.append(None)
+                newrows.append(row)
+                continue
+            
+    print str(k) + ' entries not geocoded'
+
+    nname = filename[:-4] + "_geocoded.csv" # The filename of the output file
+    header.append('Lat')
+    header.append('Lon')
+    with open(nname, "wb") as output_file:
+        writer = csv.writer(output_file)
+        writer.writerow(header)
+        writer.writerows(newrows)
+    return nname
+
+def main():
+    fname = sys.argv[1]
+    nname = combine_geocode(fname)
+    return nname
+    
+if __name__ == '__main__':
+    main()
--- mysql_python/combine_same_geolocation.py
+++ mysql_python/combine_same_geolocation.py
@@ -29,6 +29,8 @@ def combine_samegeo(filename):
         imearan = header.index('Mean_Rank')
         imedcat = header.index('Median_Cat')
         imeacat = header.index('Mean_Cat')
+        inpermits = header.index('Number_Of_Permits')
+        iavmeavio = header.index('Average_Mean_Violations_Per_Visit')
         ilat = header.index('Lat')
         ilon = header.index('Lon')
 
@@ -41,50 +43,58 @@ def combine_samegeo(filename):
                 newrows.append(row)
             else:
                 i_comb = coord.index((row[ilat], row[ilon]))
-                combid = row[iid] + ', ' + newrows[i_comb][iid]
+                combid = row[iid] + ' / ' + newrows[i_comb][iid]
                 newrows[i_comb][iid] = combid
-                combname = row[iname] + ', ' + newrows[i_comb][iname]
+                combname = row[iname] + ' / ' + newrows[i_comb][iname]
                 newrows[i_comb][iname] = combname
-                combholder = row[iholder] + ', ' + newrows[i_comb][iholder]
+                combholder = row[iholder] + ' / ' + newrows[i_comb][iholder]
                 newrows[i_comb][iholder] = combholder
-                combphone = row[iphone] + ', ' + newrows[i_comb][iphone]
+                combphone = row[iphone] + ' / ' + newrows[i_comb][iphone]
                 newrows[i_comb][iphone] = combphone
-                combpermit = row[ipermit] + ', ' + newrows[i_comb][ipermit]
+                combpermit = row[ipermit] + ' / ' + newrows[i_comb][ipermit]
                 newrows[i_comb][ipermit] = combpermit
-                combexp = row[iexp] + ', ' + newrows[i_comb][iexp]
+                combexp = row[iexp] + ' / ' + newrows[i_comb][iexp]
                 newrows[i_comb][iexp] = combexp
-                combstatus = row[istatus] + ', ' + newrows[i_comb][istatus]
+                combstatus = row[istatus] + ' / ' + newrows[i_comb][istatus]
                 newrows[i_comb][istatus] = combstatus
-                combagerange = row[iagerange] + ', ' + newrows[i_comb][iagerange]
+                combagerange = row[iagerange] + ' / ' + newrows[i_comb][iagerange]
                 newrows[i_comb][iagerange] = combagerange
-                combcap = row[icap] + ', ' + newrows[i_comb][icap]
+                combcap = row[icap] + ' / ' + newrows[i_comb][icap]
                 newrows[i_comb][icap] = combcap
-                combmed = row[imed] + ', ' + newrows[i_comb][imed]
+                combmed = row[imed] + ' / ' + newrows[i_comb][imed]
                 newrows[i_comb][imed] = combmed
-                combtype = row[itype] + ', ' + newrows[i_comb][itype]
+                combtype = row[itype] + ' / ' + newrows[i_comb][itype]
                 newrows[i_comb][itype] = combtype
-                combnvio = row[invio] + ', ' + newrows[i_comb][invio]
+                combnvio = row[invio] + ' / ' + newrows[i_comb][invio]
                 newrows[i_comb][invio] = combnvio
-                combnhaz = row[inhaz] + ', ' + newrows[i_comb][inhaz]
+                combnhaz = row[inhaz] + ' / ' + newrows[i_comb][inhaz]
                 newrows[i_comb][inhaz] = combnhaz
-                combncri = row[incri] + ', ' + newrows[i_comb][incri]
+                combncri = row[incri] + ' / ' + newrows[i_comb][incri]
                 newrows[i_comb][incri] = combncri
-                combngen = row[ingen] + ', ' + newrows[i_comb][ingen]
+                combngen = row[ingen] + ' / ' + newrows[i_comb][ingen]
                 newrows[i_comb][ingen] = combngen
-                combnvis = row[invis] + ', ' + newrows[i_comb][invis]
+                combnvis = row[invis] + ' / ' + newrows[i_comb][invis]
                 newrows[i_comb][invis] = combnvis
-                combmedvio = row[imedvio] + ', ' + newrows[i_comb][imedvio]
+                combmedvio = row[imedvio] + ' / ' + newrows[i_comb][imedvio]
                 newrows[i_comb][imedvio] = combmedvio
-                combmeavio = row[imeavio] + ', ' + newrows[i_comb][imeavio]
+                combmeavio = row[imeavio] + ' / ' + newrows[i_comb][imeavio]
                 newrows[i_comb][imeavio] = combmeavio
-                combmedran = row[imedran] + ', ' + newrows[i_comb][imedran]
+                combmedran = row[imedran] + ' / ' + newrows[i_comb][imedran]
                 newrows[i_comb][imedran] = combmedran
-                combmearan = row[imearan] + ', ' + newrows[i_comb][imearan]
+                combmearan = row[imearan] + ' / ' + newrows[i_comb][imearan]
                 newrows[i_comb][imearan] = combmearan
-                combmedcat = row[imedcat] + ', ' + newrows[i_comb][imedcat]
+                combmedcat = row[imedcat] + ' / ' + newrows[i_comb][imedcat]
                 newrows[i_comb][imedcat] = combmedcat
-                combmeacat = row[imeacat] + ', ' + newrows[i_comb][imeacat]
+                combmeacat = row[imeacat] + ' / ' + newrows[i_comb][imeacat]
                 newrows[i_comb][imeacat] = combmeacat
+
+                if row[imeavio] != '':
+                    if newrows[i_comb][iavmeavio] != '':
+                        newrows[i_comb][iavmeavio] = (int(row[inpermits])*float(row[iavmeavio]) + int(newrows[i_comb][inpermits])*float(newrows[i_comb][iavmeavio]))/(int(row[inpermits]) + int(newrows[i_comb][inpermits]))
+                        newrows[i_comb][inpermits] = int(row[inpermits]) + int(newrows[i_comb][inpermits])
+                    else:
+                        newrows[i_comb][iavmeavio] = float(row[imeavio])
+                        newrows[i_comb][inpermits] = int(newrows[i_comb][inpermits]) + 1
 
     nname = filename[:-4] + "_combinedgeo.csv" # The filename of the output file
 
