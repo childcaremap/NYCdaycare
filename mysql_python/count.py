b4aae2d9719ea30d1ec29a024c7c752cae09b27f
schmiani <schmiani@gmail.com>
2014-08-25 14:21:36 -0400

    Rewriting scripts as functions to streamline

--- mysql_python/count.py
+++ mysql_python/count.py
@@ -1,85 +1,106 @@
+import sys
 import MySQLdb
 import re
 import numpy as np
+import datetime
 
-mydb = MySQLdb.connect(host='localhost',
-    user='root',
-    passwd='password',
-    db='daycare')
-with mydb:
 
-    cur = mydb.cursor()
+def count(datescraped):
+    mydb = MySQLdb.connect(host='localhost',
+        user='root',
+        passwd='password',
+        db='daycare')
+    with mydb:
 
-    # 2 years prior to scrape date
-    mindate =  '2012-7-23'
-    #Make new table
-    cur.execute('DROP TABLE IF EXISTS BasicSum')
-    cur.execute("CREATE TABLE BasicSum LIKE Basic")
-    cur.execute("INSERT BasicSum SELECT * FROM Basic")
-    #add a column
-    cur.execute("ALTER TABLE BasicSum ADD Number_Of_Violations INT")
-    #insert sum of violations for each site
-    cur.execute("SELECT SITE_ID, COUNT(Violation_Category) FROM Inspections \
-        WHERE Violation_Category != '' AND Visit_Date >  %s GROUP BY Site_ID", (mindate,))
-    rows = cur.fetchall()
-    for row in rows:
-        cur.execute("UPDATE BasicSum SET Number_Of_Violations = %s WHERE Site_ID = %s" , (row[1],row[0]))
-    #Make NULL values into 0s
-    cur.execute("UPDATE BasicSum SET Number_Of_Violations = 0 WHERE Number_Of_Violations IS NULL")
-    mydb.commit()
+        cur = mydb.cursor()
 
-    #Add another column
-    cur.execute("ALTER TABLE BasicSum ADD Hazard_Violations INT")
+        # 2 years prior to scrape date
+        #mindate =  '2012-7-23'
+        #date = str(datescraped.isoformat())
+        scrapedminus2 = datescraped.replace(year = datescraped.year-2)
+        mindate = str(scrapedminus2.isoformat())
 
-    #insert sum of PUBLIC HEALTH HAZARD violations for each site
-    cur.execute("SELECT SITE_ID, COUNT(Violation_Category) FROM Inspections \
-        WHERE Violation_Category = 'PUBLIC HEALTH HAZARD' AND Visit_Date > %s\
-        GROUP BY Site_ID", (mindate,))
-    rows = cur.fetchall()
-    for row in rows:
-        cur.execute("UPDATE BasicSum SET Hazard_Violations = %s WHERE Site_ID = %s" , (row[1],row[0]))
-    #Make NULL values into 0s
-    cur.execute("UPDATE BasicSum SET Hazard_Violations = 0 WHERE Hazard_Violations IS NULL")
-    mydb.commit()
+        #Make new table
+        cur.execute('DROP TABLE IF EXISTS BasicSum')
+        cur.execute("CREATE TABLE BasicSum LIKE Basic")
+        cur.execute("INSERT BasicSum SELECT * FROM Basic")
+        #add a column
+        cur.execute("ALTER TABLE BasicSum ADD Number_Of_Violations INT")
+        #insert sum of violations for each site
+        cur.execute("SELECT SITE_ID, COUNT(Violation_Category) FROM Inspections \
+            WHERE Violation_Category != '' AND Visit_Date >  %s GROUP BY Site_ID", (mindate,))
+        rows = cur.fetchall()
+        for row in rows:
+            cur.execute("UPDATE BasicSum SET Number_Of_Violations = %s WHERE Site_ID = %s" , (row[1],row[0]))
+        #Make NULL values into 0s
+        cur.execute("UPDATE BasicSum SET Number_Of_Violations = 0 WHERE Number_Of_Violations IS NULL")
+        mydb.commit()
 
-    #Add another column
-    cur.execute("ALTER TABLE BasicSum ADD Critical_Violations INT")
+        #Add another column
+        cur.execute("ALTER TABLE BasicSum ADD Hazard_Violations INT")
 
-    #insert sum of critical violations for each site
-    cur.execute("SELECT SITE_ID, COUNT(Violation_Category) FROM Inspections \
-        WHERE Violation_Category = 'CRITICAL' AND Visit_Date > %s\
-        GROUP BY Site_ID", (mindate,))
-    rows = cur.fetchall()
-    for row in rows:
-        cur.execute("UPDATE BasicSum SET Critical_Violations = %s WHERE Site_ID = %s" , (row[1],row[0]))
-    #Make NULL values into 0s
-    cur.execute("UPDATE BasicSum SET Critical_Violations = 0 WHERE Critical_Violations IS NULL")
-    mydb.commit()
+        #insert sum of PUBLIC HEALTH HAZARD violations for each site
+        cur.execute("SELECT SITE_ID, COUNT(Violation_Category) FROM Inspections \
+            WHERE Violation_Category = 'PUBLIC HEALTH HAZARD' AND Visit_Date > %s\
+            GROUP BY Site_ID", (mindate,))
+        rows = cur.fetchall()
+        for row in rows:
+            cur.execute("UPDATE BasicSum SET Hazard_Violations = %s WHERE Site_ID = %s" , (row[1],row[0]))
+        #Make NULL values into 0s
+        cur.execute("UPDATE BasicSum SET Hazard_Violations = 0 WHERE Hazard_Violations IS NULL")
+        mydb.commit()
 
-    #Add another column
-    cur.execute("ALTER TABLE BasicSum ADD General_Violations INT")
+        #Add another column
+        cur.execute("ALTER TABLE BasicSum ADD Critical_Violations INT")
 
-    #insert sum of general violations for each site
-    cur.execute("SELECT SITE_ID, COUNT(Violation_Category) FROM Inspections \
-        WHERE Violation_Category = 'GENERAL' AND Visit_Date > %s\
-        GROUP BY Site_ID", (mindate,))
-    rows = cur.fetchall()
-    for row in rows:
-        cur.execute("UPDATE BasicSum SET General_Violations = %s WHERE Site_ID = %s" , (row[1],row[0]))
-    #Make NULL values into 0s
-    cur.execute("UPDATE BasicSum SET General_Violations = 0 WHERE General_Violations IS NULL")
-    mydb.commit()
+        #insert sum of critical violations for each site
+        cur.execute("SELECT SITE_ID, COUNT(Violation_Category) FROM Inspections \
+            WHERE Violation_Category = 'CRITICAL' AND Visit_Date > %s\
+            GROUP BY Site_ID", (mindate,))
+        rows = cur.fetchall()
+        for row in rows:
+            cur.execute("UPDATE BasicSum SET Critical_Violations = %s WHERE Site_ID = %s" , (row[1],row[0]))
+        #Make NULL values into 0s
+        cur.execute("UPDATE BasicSum SET Critical_Violations = 0 WHERE Critical_Violations IS NULL")
+        mydb.commit()
 
-    #Add another column
-    cur.execute("ALTER TABLE BasicSum ADD Number_Of_Visits INT")
-    #insert number of visits per site
-    cur.execute("SELECT SITE_ID, COUNT(Visit_Date) FROM InspectionSum \
-        WHERE Visit_Date > %s \
-        GROUP BY Site_ID", (mindate,))
-    rows = cur.fetchall()
-    for row in rows:
-        cur.execute("UPDATE BasicSum SET Number_Of_Visits = %s WHERE Site_ID = %s" , (row[1],row[0]))
-    cur.execute("UPDATE BasicSum SET Number_Of_Visits = 0 WHERE Number_Of_Visits IS NULL")
-    mydb.commit()
+        #Add another column
+        cur.execute("ALTER TABLE BasicSum ADD General_Violations INT")
 
-print "Done"
+        #insert sum of general violations for each site
+        cur.execute("SELECT SITE_ID, COUNT(Violation_Category) FROM Inspections \
+            WHERE Violation_Category = 'GENERAL' AND Visit_Date > %s\
+            GROUP BY Site_ID", (mindate,))
+        rows = cur.fetchall()
+        for row in rows:
+            cur.execute("UPDATE BasicSum SET General_Violations = %s WHERE Site_ID = %s" , (row[1],row[0]))
+        #Make NULL values into 0s
+        cur.execute("UPDATE BasicSum SET General_Violations = 0 WHERE General_Violations IS NULL")
+        mydb.commit()
+
+        #Add another column
+        cur.execute("ALTER TABLE BasicSum ADD Number_Of_Visits INT")
+        #insert number of visits per site
+        cur.execute("SELECT SITE_ID, COUNT(Visit_Date) FROM InspectionSum \
+            WHERE Visit_Date > %s \
+            GROUP BY Site_ID", (mindate,))
+        rows = cur.fetchall()
+        for row in rows:
+            cur.execute("UPDATE BasicSum SET Number_Of_Visits = %s WHERE Site_ID = %s" , (row[1],row[0]))
+        cur.execute("UPDATE BasicSum SET Number_Of_Visits = 0 WHERE Number_Of_Visits IS NULL")
+        mydb.commit()
+
+    print "Done Counting"
+
+def main():
+    # input date scrapes in Year-month-day or leave empty for today
+    if len(sys.argv) > 1:
+        datescraped = datetime.datetime.strptime(sys.argv[1], "%Y-%m-%d").date()
+        print "Counting last 2 years assuming data scraped on " + sys.argv[1]
+    else:
+        datescraped = datetime.date.today()
+        print "Counting last 2 years assuming data scraped today"
+    count(datescraped)
+
+if __name__ == '__main__':
+    main()
--- mysql_python/rank.py
+++ mysql_python/rank.py
@@ -1,105 +1,113 @@
+import sys
 import MySQLdb
 import re
 import numpy as np
 
-mydb = MySQLdb.connect(host='localhost',
-    user='root',
-    passwd='password',
-    db='daycare')
-with mydb:
+def rank():
+    mydb = MySQLdb.connect(host='localhost',
+        user='root',
+        passwd='password',
+        db='daycare')
+    with mydb:
 
-    cur = mydb.cursor()
-    
-    #Add 2 more columns if necessary
-    if cur.execute("SHOW COLUMNS FROM BasicSum WHERE Field = 'Mean_Rank'") != 0:
-        cur.execute("ALTER TABLE BasicSum DROP Mean_Rank")
-    cur.execute("ALTER TABLE BasicSum ADD Mean_Rank INT")
-
-    if cur.execute("SHOW COLUMNS FROM BasicSum WHERE Field = 'Mean_Cat'") != 0:
-        cur.execute("ALTER TABLE BasicSum DROP Mean_Cat")
-    cur.execute("ALTER TABLE BasicSum ADD Mean_Cat VARCHAR(10)")
-
-    #Add 2 more columns if necessary
-    if cur.execute("SHOW COLUMNS FROM BasicSum WHERE Field = 'Score_Rank'") != 0:
-        cur.execute("ALTER TABLE BasicSum DROP Score_Rank")
-    cur.execute("ALTER TABLE BasicSum ADD Score_Rank INT")
-
-    if cur.execute("SHOW COLUMNS FROM BasicSum WHERE Field = 'Score_Cat'") != 0:
-        cur.execute("ALTER TABLE BasicSum DROP Score_Cat")
-    cur.execute("ALTER TABLE BasicSum ADD Score_Cat VARCHAR(10)")
+        cur = mydb.cursor()
         
+        #Add 2 more columns if necessary
+        if cur.execute("SHOW COLUMNS FROM BasicSum WHERE Field = 'Mean_Rank'") != 0:
+            cur.execute("ALTER TABLE BasicSum DROP Mean_Rank")
+        cur.execute("ALTER TABLE BasicSum ADD Mean_Rank INT")
 
-    #select mean number of violations per visit
-    cur.execute("SELECT SITE_ID, Mean_Violations_Per_Visit, Score FROM BasicSum")
-    rows = cur.fetchall()
-    matrix = np.asarray(rows)
-    means = np.asarray(matrix[:,1],dtype=np.float64)
-    scores = np.asarray(matrix[:,2],dtype=np.float64)
+        if cur.execute("SHOW COLUMNS FROM BasicSum WHERE Field = 'Mean_Cat'") != 0:
+            cur.execute("ALTER TABLE BasicSum DROP Mean_Cat")
+        cur.execute("ALTER TABLE BasicSum ADD Mean_Cat VARCHAR(10)")
 
-    means_nonan = np.delete(means,np.nonzero(np.isnan(means)==True))
-    scores_nonan = np.delete(scores,np.nonzero(np.isnan(scores)==True))
-    
-    temp = means.argsort()
-    meanranks = np.empty(len(means), int)
-    meanranks[temp] = np.arange(len(means))
+        #Add 2 more columns if necessary
+        if cur.execute("SHOW COLUMNS FROM BasicSum WHERE Field = 'Score_Rank'") != 0:
+            cur.execute("ALTER TABLE BasicSum DROP Score_Rank")
+        cur.execute("ALTER TABLE BasicSum ADD Score_Rank INT")
 
-    temp = scores.argsort()
-    scoreranks = np.empty(len(scores), int)
-    scoreranks[temp] = np.arange(len(scores))
+        if cur.execute("SHOW COLUMNS FROM BasicSum WHERE Field = 'Score_Cat'") != 0:
+            cur.execute("ALTER TABLE BasicSum DROP Score_Cat")
+        cur.execute("ALTER TABLE BasicSum ADD Score_Cat VARCHAR(10)")
+            
 
-    quantile_means = np.percentile(means_nonan,(5,15,30,60,85,95))
-    f = open('quantiles_means.txt', 'w')
-    f.write('cdfs: ' + str([5,15,30,60,85,95]) + '\n')
-    f.write('quantiles: ' + str(quantile_means))
-    f.close()
+        #select mean number of violations per visit
+        cur.execute("SELECT SITE_ID, Mean_Violations_Per_Visit, Score FROM BasicSum")
+        rows = cur.fetchall()
+        matrix = np.asarray(rows)
+        means = np.asarray(matrix[:,1],dtype=np.float64)
+        scores = np.asarray(matrix[:,2],dtype=np.float64)
 
-    quantile_scores = np.percentile(scores_nonan,(5,15,30,60,85,95))
-    f = open('quantiles_scores.txt', 'w')
-    f.write('cdfs: ' + str([5,15,30,60,85,95]) + '\n')
-    f.write('quantiles: ' + str(quantile_scores))
-    f.close()
-
-    for i,site in enumerate(matrix[:,0]):
-        if ~np.isnan(means[i]):
-            cur.execute("UPDATE BasicSum SET Mean_Rank = %s WHERE Site_ID = %s" , (meanranks[i],site))
-
-            if means[i] <= quantile_means[0]:
-                cur.execute("UPDATE BasicSum SET Mean_Cat = %s WHERE Site_ID = %s" , ('Top 5%',site))
-            elif means[i] > quantile_means[0] and means[i] <= quantile_means[1]:
-                cur.execute("UPDATE BasicSum SET Mean_Cat = %s WHERE Site_ID = %s" , ('Top 15%',site))
-            elif means[i] > quantile_means[1] and means[i] <= quantile_means[2]:
-                cur.execute("UPDATE BasicSum SET Mean_Cat = %s WHERE Site_ID = %s" , ('Top 30%',site))
-            elif means[i] > quantile_means[3] and means[i] <= quantile_means[4]:
-                cur.execute("UPDATE BasicSum SET Mean_Cat = %s WHERE Site_ID = %s" , ('Bottom 30%',site))
-            elif means[i] > quantile_means[4] and means[i] <= quantile_means[5]:
-                cur.execute("UPDATE BasicSum SET Mean_Cat = %s WHERE Site_ID = %s" , ('Bottom 15%',site))
-            elif means[i] > quantile_means[5]:
-                cur.execute("UPDATE BasicSum SET Mean_Cat = %s WHERE Site_ID = %s" , ('Bottom 5%',site))
-            else:
-                cur.execute("UPDATE BasicSum SET Mean_Cat = %s WHERE Site_ID = %s" , ('Average',site))
-        else:
-            cur.execute("UPDATE BasicSum SET Mean_Cat = %s WHERE Site_ID = %s" , ('No Data',site))
-
-        if ~np.isnan(scores[i]):
-            cur.execute("UPDATE BasicSum SET Score_Rank = %s WHERE Site_ID = %s" , (scoreranks[i],site))
-
-            if scores[i] <= quantile_scores[0]:
-                cur.execute("UPDATE BasicSum SET Score_Cat = %s WHERE Site_ID = %s" , ('Top 5%',site))
-            elif scores[i] > quantile_scores[0] and scores[i] <= quantile_scores[1]:
-                cur.execute("UPDATE BasicSum SET Score_Cat = %s WHERE Site_ID = %s" , ('Top 15%',site))
-            elif scores[i] > quantile_scores[1] and scores[i] <= quantile_scores[2]:
-                cur.execute("UPDATE BasicSum SET Score_Cat = %s WHERE Site_ID = %s" , ('Top 30%',site))
-            elif scores[i] > quantile_scores[3] and scores[i] <= quantile_scores[4]:
-                cur.execute("UPDATE BasicSum SET Score_Cat = %s WHERE Site_ID = %s" , ('Bottom 30%',site))
-            elif scores[i] > quantile_scores[4] and scores[i] <= quantile_scores[5]:
-                cur.execute("UPDATE BasicSum SET Score_Cat = %s WHERE Site_ID = %s" , ('Bottom 15%',site))
-            elif scores[i] > quantile_scores[5]:
-                cur.execute("UPDATE BasicSum SET Score_Cat = %s WHERE Site_ID = %s" , ('Bottom 5%',site))
-            else:
-                cur.execute("UPDATE BasicSum SET Score_Cat = %s WHERE Site_ID = %s" , ('Average',site))
-        else:
-            cur.execute("UPDATE BasicSum SET Score_Cat = %s WHERE Site_ID = %s" , ('No Data',site))
+        means_nonan = np.delete(means,np.nonzero(np.isnan(means)==True))
+        scores_nonan = np.delete(scores,np.nonzero(np.isnan(scores)==True))
         
-    mydb.commit()
+        temp = means.argsort()
+        meanranks = np.empty(len(means), int)
+        meanranks[temp] = np.arange(len(means))
 
-print "Done"
+        temp = scores.argsort()
+        scoreranks = np.empty(len(scores), int)
+        scoreranks[temp] = np.arange(len(scores))
+
+        quantile_means = np.percentile(means_nonan,(5,15,30,60,85,95))
+        f = open('quantiles_means.txt', 'w')
+        f.write('cdfs: ' + str([5,15,30,60,85,95]) + '\n')
+        f.write('quantiles: ' + str(quantile_means))
+        f.close()
+
+        quantile_scores = np.percentile(scores_nonan,(5,15,30,60,85,95))
+        f = open('quantiles_scores.txt', 'w')
+        f.write('cdfs: ' + str([5,15,30,60,85,95]) + '\n')
+        f.write('quantiles: ' + str(quantile_scores))
+        f.close()
+
+        for i,site in enumerate(matrix[:,0]):
+            if ~np.isnan(means[i]):
+                cur.execute("UPDATE BasicSum SET Mean_Rank = %s WHERE Site_ID = %s" , (meanranks[i],site))
+
+                if means[i] <= quantile_means[0]:
+                    cur.execute("UPDATE BasicSum SET Mean_Cat = %s WHERE Site_ID = %s" , ('Top 5%',site))
+                elif means[i] > quantile_means[0] and means[i] <= quantile_means[1]:
+                    cur.execute("UPDATE BasicSum SET Mean_Cat = %s WHERE Site_ID = %s" , ('Top 15%',site))
+                elif means[i] > quantile_means[1] and means[i] <= quantile_means[2]:
+                    cur.execute("UPDATE BasicSum SET Mean_Cat = %s WHERE Site_ID = %s" , ('Top 30%',site))
+                elif means[i] > quantile_means[3] and means[i] <= quantile_means[4]:
+                    cur.execute("UPDATE BasicSum SET Mean_Cat = %s WHERE Site_ID = %s" , ('Bottom 30%',site))
+                elif means[i] > quantile_means[4] and means[i] <= quantile_means[5]:
+                    cur.execute("UPDATE BasicSum SET Mean_Cat = %s WHERE Site_ID = %s" , ('Bottom 15%',site))
+                elif means[i] > quantile_means[5]:
+                    cur.execute("UPDATE BasicSum SET Mean_Cat = %s WHERE Site_ID = %s" , ('Bottom 5%',site))
+                else:
+                    cur.execute("UPDATE BasicSum SET Mean_Cat = %s WHERE Site_ID = %s" , ('Average',site))
+            else:
+                cur.execute("UPDATE BasicSum SET Mean_Cat = %s WHERE Site_ID = %s" , ('No Data',site))
+
+            if ~np.isnan(scores[i]):
+                cur.execute("UPDATE BasicSum SET Score_Rank = %s WHERE Site_ID = %s" , (scoreranks[i],site))
+
+                if scores[i] <= quantile_scores[0]:
+                    cur.execute("UPDATE BasicSum SET Score_Cat = %s WHERE Site_ID = %s" , ('Top 5%',site))
+                elif scores[i] > quantile_scores[0] and scores[i] <= quantile_scores[1]:
+                    cur.execute("UPDATE BasicSum SET Score_Cat = %s WHERE Site_ID = %s" , ('Top 15%',site))
+                elif scores[i] > quantile_scores[1] and scores[i] <= quantile_scores[2]:
+                    cur.execute("UPDATE BasicSum SET Score_Cat = %s WHERE Site_ID = %s" , ('Top 30%',site))
+                elif scores[i] > quantile_scores[3] and scores[i] <= quantile_scores[4]:
+                    cur.execute("UPDATE BasicSum SET Score_Cat = %s WHERE Site_ID = %s" , ('Bottom 30%',site))
+                elif scores[i] > quantile_scores[4] and scores[i] <= quantile_scores[5]:
+                    cur.execute("UPDATE BasicSum SET Score_Cat = %s WHERE Site_ID = %s" , ('Bottom 15%',site))
+                elif scores[i] > quantile_scores[5]:
+                    cur.execute("UPDATE BasicSum SET Score_Cat = %s WHERE Site_ID = %s" , ('Bottom 5%',site))
+                else:
+                    cur.execute("UPDATE BasicSum SET Score_Cat = %s WHERE Site_ID = %s" , ('Average',site))
+            else:
+                cur.execute("UPDATE BasicSum SET Score_Cat = %s WHERE Site_ID = %s" , ('No Data',site))
+            
+        mydb.commit()
+
+    print "Done Ranking"
+
+def main():
+    rank()
+
+if __name__ == '__main__':
+    main()
